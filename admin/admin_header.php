a1ca1824fe44cdc9091b4db74fc9223fc623f3a2
DwikiAprilianAryanda <181479443+DwikiAprilianAryanda@users.noreply.github.com>
2025-11-28 06:47:38 +0700

    revisi tampilan beranda

--- admin/admin_header.php
+++ admin/admin_header.php
@@ -120,7 +120,7 @@ function is_active($page, $current_page) {
         <div class="sidebar-footer">
             <a href="logout.php" class="nav-item logout">
                 <span class="icon"><svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path><polyline points="16 17 21 12 16 7"></polyline><line x1="21" y1="12" x2="9" y2="12"></line></svg></span>
-                <span class="label">Logout</span>
+                <span class="label">Keluar</span>
             </a>
         </div>
     </aside>
