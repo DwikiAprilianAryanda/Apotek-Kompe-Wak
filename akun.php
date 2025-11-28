e4813dca9e466e29a0d7f56d09b527b7ab2760c4
DwikiAprilianAryanda <181479443+DwikiAprilianAryanda@users.noreply.github.com>
2025-11-28 07:12:36 +0700

    fix bug akun

--- akun.php
+++ akun.php
@@ -55,6 +55,15 @@ if (!empty($user['profile_pic'])) {
 
                 <div class="profile-menu-group">
                     
+                    <a href="akun.php" class="btn-menu-item active">
+                        <div class="menu-icon">
+                            <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
+                        </div>
+                        <span>Biodata Diri</span>
+                        <div class="menu-arrow">
+                            <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="9 18 15 12 9 6"></polyline></svg>
+                        </div>
+                    </a>
                     <a href="riwayat_pesanan.php" class="btn-menu-item">
                         <div class="menu-icon">
                             <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
